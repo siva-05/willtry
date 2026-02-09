59b33ae42c1125cc759a89e527be7be060054019
SIVARAMA KRISHNAN S <sivakaar2002@gmail.com>
2026-02-09 16:24:22 +0530

    Update axios.js

--- will_try_forGithub/frontend/src/lib/axios.js
+++ will_try_forGithub/frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://willtry-backend.onrender.com/api",
-    baseURL: import.meta.env.MODE === "https://willtry-backend.onrender.com/api",
+    baseURL: "https://willtry-backend.onrender.com/api",
   withCredentials: true, 
 });
 
@@ -10,4 +10,5 @@ export const axiosInstance = axios.create({
 
 
  
+
 
