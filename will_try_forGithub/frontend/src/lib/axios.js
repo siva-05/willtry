1375d4a74769fe385de7d154e27c72d92e9ad08b
SIVARAMA KRISHNAN S <sivakaar2002@gmail.com>
2026-02-09 15:58:55 +0530

    Update axios.js

--- will_try_forGithub/frontend/src/lib/axios.js
+++ will_try_forGithub/frontend/src/lib/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://willtry-backend.onrender.com/api",
   withCredentials: true, 
 });
 
   // send the cookies with req
+
 
  
